3d01625c022ca54b62769432029ffc7e46726f85
santidotpy <santidotpy@gmail.com>
2023-04-21 20:01:15 -0300

    🐛 fix request

--- src/memes.py
+++ src/memes.py
@@ -1,7 +1,7 @@
 import requests
 
 def meme():
-    r = requests.get('https://meme-api.herokuapp.com/gimme')
+    r = requests.get('https://meme-api.com/gimme')
     response = r.json()
     meme_img = response['url']
     return meme_img
